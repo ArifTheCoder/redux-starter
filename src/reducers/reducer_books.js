e26e52d0e0c24ea0685d7c605d237ac3f8d7ea13
Arif Hossain <arifthecoder@gmail.com>
2018-08-27 09:44:28 +0300

    Initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title: 'Programming Language in C', pages: 246},
+        {title: 'Python is a Programming Language', pages: 373},
+        {title: 'Javascript: The Good Parts', pages: 134},
+        {title: 'React with Redux', pages: 176}
+    ]
+}
